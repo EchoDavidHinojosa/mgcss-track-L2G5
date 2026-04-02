3364054aeff8bdfc0f097c707431b89eb7314739
EchoDavidHinojosa <davidhinojosaecho@gmail.com>
2026-04-02 17:53:37 +0200

    La última

--- src/main/java/mantenimiento/practica/PracticaApplication.java
+++ src/main/java/mantenimiento/practica/PracticaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PracticaApplication.class, args);
+	//	SpringApplication.run(PracticaApplication.class, args);
 	}
 
 }
